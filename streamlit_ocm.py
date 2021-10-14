32f98cef553e552bf02b6ce0a3ecd3d545f3301c
Omer Erdem <omer.erdem@hva.nl>
2021-10-14 10:44:42 +0200

    streamlit attempt #5

--- streamlit_ocm.py
+++ streamlit_ocm.py
@@ -8,4 +8,18 @@ import plotly.express as px
 import geopandas as gpd
 
 def main():
-    st.markdown("Dit is Open Charge Map")
+    st.markdown("### Introductie")
+    st.markdown("Voor het ophalen van de laadpalen data, hebben we gebruik gemaakt van de Open Charge Map "
+                "API. De API kan je terug vinden op [OpenChargeMap](https://openchargemap.org/site/develop/api). "
+                "Tijdens het ophalen hebben we opgemerkt dat we niet alle data terugkregen, dus hiervoor hebben "
+                "gekozen om de maximum resultaat limiet op 100.000 te zetten om het analyse zo goed mogelijk "
+                "uit te voeren.")
+
+    st.markdown("### Analyse")
+
+
+    st.markdown("### Cleaning")
+
+
+    st.markdown("### Plots")
+
