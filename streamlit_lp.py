e4818b6a50324aad8e4faf3a6dd6bdc4e44ae443
Max van Dam <45095472+maxvandam@users.noreply.github.com>
2021-10-14 10:38:14 +0200

    Update streamlit_lp.py

--- streamlit_lp.py
+++ streamlit_lp.py
@@ -1,4 +1,26 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
+import fuzzywuzzy
+import seaborn as sns
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+
+
 
 def main():
-    st.markdown("Dit is Laadpalen")
+    st.title("Laadpaal Data")
+    st.header("###Importeren en opschonen van data")
+    paalData = pd.read_csv('laadpaaldata.csv')
+    paalData.head()
+    st.code("paalData = pd.read_csv('laadpaaldata.csv')
+    "\npaalData.head()")
+    plt.figure()
+    sns.scatterplot(x='ChargeTime', y='ConnectedTime', data=paalData)
+    st.plotly_chart(fig)
+
+
+
+
