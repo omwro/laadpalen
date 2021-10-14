1da8f46003720a8a5bada1ca11a7eae37977e3c1
Max van Dam <45095472+maxvandam@users.noreply.github.com>
2021-10-14 11:01:49 +0200

    Update streamlit_lp.py

--- streamlit_lp.py
+++ streamlit_lp.py
@@ -9,13 +9,25 @@ import plotly.figure_factory as ff
 
 def main():
     st.title("Laadpaal Data")
-    st.header("###Importeren en opschonen van data")
+    st.header("Importeren en opschonen van data")
     paalData = pd.read_csv('laadpaaldata.csv')
     paalData.head()
     st.code("paalData = pd.read_csv('laadpaaldata.csv')"
     "\npaalData.head()")
-    fig = px.scatter(x=paalData['ChargeTime'],y=paalData['ConnectedTime'])
+    fig = px.scatter(x=paalData['ChargeTime'],y=paalData['ConnectedTime'],labels={'x':'Laadtijd','y':'Aansluit tijd'})
     st.plotly_chart(fig)
+    st.caption("Er is hier te zien dat er duidelijke outliers zijn in de laadtijd, deze worden gefilterd door middel van:")
+    st.code("paalData = paalData[paalData['ChargeTime'] > 0]"
+            "\npaalData = paalData[paalData['ChargeTime'] < 7]")
+
+    st.caption("We houden nu het volgende DataFrame over:")
+    st.dataframe(paalData)
+
+    with st.echo():
+        st.write("Laat de gemiddelden van de kolommen zien:")
+        st.write(paalData.mean())
+
+
 
 
 
