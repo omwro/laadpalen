671bdb205eb12f8891f9d00430cef35d3cffc00d
Omer Erdem <omer.erdem@hva.nl>
2021-10-14 10:05:19 +0200

    streamlit attempt #1

---
+++ streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import requests as req
+from IPython.display import display
+from fuzzywuzzy import fuzz
+import plotly.express as px
+import geopandas as gpd
+
+
+st.title("Laadpalen case")
